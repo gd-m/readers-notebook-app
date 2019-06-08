dfde2e48310f296db6193208dc358a7ec67a8725
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-08 15:45:58 -0500

    added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :email, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true
+	validates :password,  presence: true, length: {6..128}
 end
