8410b47b9a00516e3a736ea3ddc6a780c5001d14
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-09 13:49:17 -0500

    setup sign up form controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+	helpers do 
+
+		def current_user
+			@current_user ||= User.find_by(id: seesion[:id])
+		end
+
+		def user_signed_in?
+			!!current_user
+			
+		end
+
+		
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
 
+	#signup get and post request as new and create respectively. 
+	#load the signup form
 	def new
+		@user = User.new
+	end
+	# signup and set the session and login the user
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			#login the user
+			session[:id] = params[:id]
+			#redirect to user home page
+			redirect_to home_path
+
+		else
+			render :new
+			
+		end
+		
+	end
+
+	def home
+		
+		
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
 		
 	end
 
